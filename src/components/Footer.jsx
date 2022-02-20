b99cfce208ec36e32670e5be2217a51c4c7cad4b
Akiacode <catry.me@gmail.com>
2022-02-21 03:06:35 +0900

    add manifest

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,13 +3,13 @@ import styles from '../App.module.css'
 export default function Footer() {
     return (
     <footer className={styles.footer} role="contentinfo">
-        <div className={styles.license}>
+        { /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ? null :<div className={styles.license}>
             Inspired by <a className={styles.licenseLink} href='https://www.nytimes.com/games/wordle/index.html'>Wordle</a>
             <br/>
             There is no age, race, or gender in <a className={styles.licenseLink} href='https://www.khanacademy.org/'>learning</a>.
             <br/>
             Princeton University "About WordNet." <a className={styles.licenseLink} href='https://wordnet.princeton.edu/'>WordNet</a>. Princeton University. 2010.
-        </div>
+        </div>}
         Copyright 2022. Ratry. All rights reserved.
     </footer>
   )
--- vite.config.mjs
+++ vite.config.mjs
@@ -6,8 +6,24 @@ import { VitePWA } from 'vite-plugin-pwa';
 export default defineConfig({
   plugins: [solidPlugin({ adapter: vercel() }), VitePWA({
     manifest: {
+      short_name: 'dictry',
       name: 'Dictry',
       description: "Let's memorize the words for a day!",
+      start_url: '/',
+      background_color: '#1d1d1d',
+      display: 'standalone',
+      lang: 'en',
+      theme_color: '#ee7f2d',
+      icons: [
+        {
+          "src": "./src/assets/dictry.png",
+          "sizes": "512x512",
+          "type": "image/png"
+        }
+      ]
     },
+    workbox: {
+      swDest: 'service-worker.js',
+    }
   })]
 });

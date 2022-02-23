56370dbba067ef81f049b29f003cfa0e79fbb814
Akiacode <catry.me@gmail.com>
2022-02-24 05:01:33 +0900

    maybe fix

--- src/components/GameOverPopup.jsx
+++ src/components/GameOverPopup.jsx
@@ -3,7 +3,7 @@ import { lazy, createSignal, onCleanup, createEffect } from 'solid-js'
 const Popup = lazy(() => import('../components/Popup'))
 
 export default function GameOverPopup(props /** nextday: Number, isGameOver: 'win' or 'lose' */) {
-    const [CountDown, setCountDown] = createSignal( props.nextday - new Date().getTime())
+    const [CountDown, setCountDown] = createSignal(props.nextday - new Date().valueOf())
 
     const timer = setInterval(() => setCountDown(CountDown() - 1000), 1000)
     onCleanup(() => { clearInterval(timer) })
